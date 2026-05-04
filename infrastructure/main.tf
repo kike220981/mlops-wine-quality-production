052fc0af1997616b35ed219d57b6087b0cb15beb
Enrique Sanchez Antuña <150960757+kike220981@users.noreply.github.com>
2026-05-04 16:07:21 +0000

    infra: add terraform basic configuration[cite: 1, 2]

---
+++ infrastructure/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "wine_api" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type = "t2.micro"
+  tags = { Name = "WineQualityAPI" }
+}
